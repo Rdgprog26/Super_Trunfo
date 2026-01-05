19e6ef6f94a71bfc170e39af3d38f0d824b1967a
Rdgprog26 <rodrigofk00@hotmail.com>
2026-01-05 20:05:00 +0000

    acrescentei 2 variaveis por meio do calculo de divisão de outras 2 variaveis já existentes

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -13,18 +13,25 @@ int main() {
   char Estado_1 = 'A';
   char Código_1[20] = "A01";
   char Nome_Cidade_1 [20] = "Tóquio";
-  int Populacão_1 = 13503810;
+  int Populacão_1 = 1350810;
   float Área_1 = 2189.08;
-  float PIB_1 = 868.6;
+  float PIB_1 = 868000000.00;
   int Número_de_Pontos_Turísticos_1 = 250;
 
   char Estado_2 = 'B';
-  char Código_2[20] = "A02";
+  char Código_2[20] = "B02";
   char Nome_Da_Cidade_2 [20] = "Veneza";
   int Populacão_2 = 249466;
   float Área_2 = 414.0;
-  float PIB_2 = 45.0;
+  float PIB_2 = 450000000.00;
   int Número_de_Pontos_Turísticos_2 = 190;
+  
+  // Calculando o PIB per capita e a Densidade Populacional utilizando a Divisão
+
+  float Densidade_populacional_A01 = Populacão_1 / Área_1;
+  float PIB_Per_Capita_A01 = PIB_1 / Populacão_1;
+  float Densidade_populacional_B02 = Populacão_2 / Área_2;
+  float PIB_Per_Capita_B02 = PIB_2 / Populacão_2;
 
   // Área para entrada de dados
 
@@ -41,17 +48,21 @@ int main() {
   printf ("Cidade: %s\n", Nome_Cidade_1);
   printf ("População: %d\n", Populacão_1);
   printf ("Área por Km²: %.2f\n", Área_1);
-  printf ("PIB em Bilhões: %.2f\n", PIB_1);
+  printf ("PIB: %.2f\n", PIB_1);
   printf ("Pontos Turísticos: %d\n", Número_de_Pontos_Turísticos_1);
+  printf ("Densidade Populacional: %.2f\n",Densidade_populacional_A01);
+  printf ("PIB Per Capita: %.2f\n", PIB_Per_Capita_A01);
 
   printf ("Carta 2: %c\n", Estado_2);
   printf ("Código da Carta: %s\n", Código_2);
   printf ("Cidade: %s\n", Nome_Da_Cidade_2);
   printf ("População: %d\n", Populacão_2);
   printf ("Área por Km²: %.2f\n", Área_2);
-  printf ("PIB Em Bilhões: %.2f\n", PIB_2);
+  printf ("PIB: %.2f\n", PIB_2);
   printf ("Pontos Turísticos: %d\n", Número_de_Pontos_Turísticos_2);
-
+  printf ("Densidade Populacional: %.2f\n",Densidade_populacional_B02);
+  printf ("PIB Per Capita: %.2f\n", PIB_Per_Capita_B02);
+  
   // Área para exibição dos dados da cidade
 
 return 0;
